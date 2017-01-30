12a84afbb6dc3de35113c54e6cbd211dfe083ecb
Benjamin Friesen <bfriesen95@gmail.com>
2017-01-30 11:11:08 -0800

    layout and keyboard

--- scripts/keyboard.js
+++ scripts/keyboard.js
@@ -1,34 +1,33 @@
 function Keyboard(toUpdate = []) {
-    
+
+    this.pressed = [];
     this.active = [];
+    this.released = [];
     this.toUpdate = toUpdate;
-    
+
     this.down = function (event) {
         for (var i = 0; i < this.active.length; i++) {
-            if (this.active[i] === event.keyCode) {
+            if (this.active[i] === event.keycode) {
                 return;
             }
         }
-        this.active.push(event.keyCode);
+        this.active.push(event.keycode);
         this.update();
     }
-    
-    this.up = function(event) {
+
+    this.up = function (event) {
         for (var i = 0; i < this.active.length; i++) {
-            if (this.active[i] === event.keyCode) {
+            if (this.active[i] === event.keycode) {
                 this.active.splice(i, 1);
                 this.update();
                 return;
             }
         }
     }
-    
-    this.update = function() {
+
+    this.update = function () {
         for (var i = 0; i < this.toUpdate.length; i++) {
             this.toUpdate[i].update(this.active);
         }
     }
 }
-
-
-
