4a84551de4888dc2faa3d0305870192c658fbaec
unknown <bfriesen95@gmail.com>
2017-02-02 23:24:50 -0800

    main cleanup

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,5 @@ var combo = new Combo("🍆", ":", 5, document.getElementById(COMBO));
 var kts = new KeyToScreen();
 var keyboard = new Keyboard([combo, kts]);
 
-// document.addEventListener('keydown', keyboard.down);
-// document.addEventListener('keyup', keyboard.up);
-
-document.onkeydown = function myFunction(event){ keyboard.down(event); };
-document.onkeyup = function myFunction(event){ keyboard.up(event); };
+document.onkeydown = function (event) { keyboard.down(event); };
+document.onkeyup = function (event) { keyboard.up(event); };
